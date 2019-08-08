9d685bce03a5562c503a44df777020c759b51e37
Arturo Sotelo Bayangos <ambibayangos@gmail.com>
2019-08-08 13:39:24 +1200

    add hover effect on add video panel

---
+++ front-end/webapp/src/components/BackDrop/backDrop.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./backDrop.css";
+
+class BackDrop extends Component {
+  render() {
+    return <div className="backDrop" />;
+  }
+}
+
+export default BackDrop;
