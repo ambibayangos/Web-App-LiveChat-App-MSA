5af12bfd602f247dc1055238bbaaebd4e0329f83
Arturo Sotelo Bayangos <ambibayangos@gmail.com>
2019-08-10 02:16:11 +1200

    installed signalR dependencies

--- front-end/webapp/src/components/ChatBox/chatBox.jsx
+++ front-end/webapp/src/components/ChatBox/chatBox.jsx
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import "./chatBox.css";
 import "semantic-ui-css/semantic.min.css";
 import { Form } from "semantic-ui-react";
+import { HubConnection } from "@aspnet/signalr-client";
 
 class ChatBox extends Component {
-  state = { message: "", messageList: [] };
+  state = { message: "", messageList: [], hubConnection: null };
 
   onSubmitHandler = event => {
     document.getElementById("message").reset();
@@ -14,13 +15,25 @@ class ChatBox extends Component {
     });
   };
 
+  componentDidMount() {
+    const hubConnection = new HubConnection("http://localhost:5000/ChatHub");
+    this.setState({ hubConnection: hubConnection });
+
+    this.setState({ hubConnection }, () => {
+      this.state.hubConnection
+        .start()
+        .then(() => console.log("Connection started!"))
+        .catch(err => console.log("Error while establishing connection :("));
+    });
+  }
+
   render() {
     return (
       <div className="chatBox">
         <div>
-          {this.state.messageList.slice(-11).map(message => (
-            <ul style={{ color: "white", fontSize: "1rem" }}>
-              <span style={{ fontSize: "1rem" }}>
+          {this.state.messageList.slice(-9).map(message => (
+            <ul style={{ color: "white", fontSize: "1rem", padding: "10px" }}>
+              <span style={{ fontSize: "1rem", color: "red" }}>
                 {this.props.userName + " : "}
               </span>
               {message}
